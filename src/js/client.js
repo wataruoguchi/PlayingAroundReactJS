0fd0060d1033ac56e5b0fe9abb1dcf7fd8685d9f
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2016-04-03 20:30:26 -0700

    By Tutorial #3

--- src/js/client.js
+++ src/js/client.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "Will";
-  }
-  render() {
-    return (
-      <h1>It works, {this.name}!</h1>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
-
 ReactDOM.render(<Layout/>, app);
---
+++ src/js/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Title from "./Header/Title"
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <Title />
+        <header>Header</header>
+      </div>
+    );
+  }
+}
