da676021d218fa539f8df1c8dfb1b5699ad6cc5c
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2016-04-03 21:28:01 -0700

    By totorial #5

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,11 +3,15 @@ import React from "react";
 import Title from "./Header/Title"
 
 export default class Header extends React.Component {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
   render() {
     return (
       <div>
         <Title title={this.props.title} />
-        <header>Header</header>
+        <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
       </div>
     );
   }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -10,13 +10,15 @@ export default class Layout extends React.Component {
       title: "Welcome",
     };
   }
+
+  changeTitle(title) {
+    this.setState({title})
+  }
+
   render() {
-    setTimeout(() => {
-      this.setState({title: "Welcome Will!"})
-    }, 2000);
     return (
       <div>
-        <Header title={this.state.title}/>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer />
       </div>
     );
